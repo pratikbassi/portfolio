eb5c09d05e1eb0429fcc31866cee4a7ca460ea00
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-07-10 02:06:01 -0700

    began creating api for receiving data

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+
+
+def index(request):
+    data = {}
+
+    return JsonResponse("Hello, world. You're at the polls index.")
