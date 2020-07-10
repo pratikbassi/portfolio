a05727bbd7b9b9c3c72fb10096dd2713ab5fa29b
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-07-10 03:31:07 -0700

    finished server side API for creating, fetching guest locations and email inquiries

--- api/views.py
+++ api/views.py
@@ -2,9 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import JsonResponse
+from api.models import EmailRequest, VisitorLocation
 
+def newEmail(request):
 
-def index(request):
-    data = {}
+    newEmail = EmailRequest.objects.create()
+    newEmail.name = request.name
+    newEmail.email = request.email
+    newEmail.subject = request.subject
+    newEmail.message = request.message
+    newEmail.save()
 
-    return JsonResponse("Hello, world. You're at the polls index.")
+    return JsonResponse(200)
+
+def newGuest(request):
+    data = {
+        "locationX": request.locationX,
+        "locationY": request.locationY
+    }
+
+    newGuest = VisitorLocation.objects.create()
+    newGuest.locationX = request.locationX
+    newGuest.locationY = request.locationY
+    newGuest.save()
+
+    return JsonResponse(data)
+
+def allGuests(request):
+    guests = VisitorLocation.objects.get()
+    return JsonResponse(guests)
