b3ad6d7624fc09c4bce127179e3b983f0bab0a14
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-07-10 17:02:11 -0700

    began writing jsx for web layout

---
+++ frontend/src/homepage/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function () {
+
+
+
+    return (
+        <div className='app-header'>
+            <h1>Pratik Bassi</h1>
+        </div>
+    )
+}
+
