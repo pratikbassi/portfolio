48cebe2b73c097e641130e65206e13341553f55e
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-07-10 17:39:47 -0700

    added form IPR to newEmail.js

--- frontend/src/homepage/body/newEmail.js
+++ frontend/src/homepage/body/newEmail.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 export default function () {
+    let [state, setState] = useState(
+        {name: '', email: '', subject: '', message: ''}
+    )
+
+
     return (
         <div className='email-form'>
             <form>
                 <label>
                     Name:
-                    <input type="text" name="name"/>
+                    <input type="text" value={state.name} name="name" required/>
+                    <input type="email" value={state.email} name="email" required/>
+                    <input type="text" value={state.subject} name="subject" required/>
+                    <input type="text" value={state.message}  name="message" required/>
                 </label>
-                <input type="submit" value="Submit"/>
+                <button type="button" />
             </form>
         </div>
     )
