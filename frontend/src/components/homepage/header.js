1d54b86e53bc0977d89d87e91696781bd45a3ca3
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-07-20 01:21:28 -0700

    added css

--- frontend/src/components/homepage/header.js
+++ frontend/src/components/homepage/header.js
@@ -6,19 +6,16 @@ export default function () {
 
 
     return (
-        <ul className='app-header'>
+        <div className='app-header'>
 
-            <li>
-                <h1 className='header-title'>Pratik Bassi</h1>
-            </li>
-            <li>
+            <h1 className='header-title'>Pratik Bassi</h1>
+            <div className='right-side'>
                 <p className='header-top'>TODO:back to top</p>
-            </li>
-            <li>
                 <p className='header-bottom'>TODO:view resume</p>
-            </li>
+            </div>
 
-        </ul>
+
+        </div>
     )
 }
 
