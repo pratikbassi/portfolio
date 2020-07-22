7cee8486421d704cadc54cd72286f278ea3bca55
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-07-21 22:42:30 -0700

    added modals to homepage

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,6 +3,7 @@
 
 import React from 'react';
 import Header from './homepage/header'
+import Body from './homepage/body/index'
 
 
 export default function App () {
@@ -11,6 +12,7 @@ export default function App () {
     return (
         <div className='frontpage'>
             <Header />
+            <Body />
         </div>
     )
 }
--- frontend/src/components/homepage/header.js
+++ frontend/src/components/homepage/header.js
@@ -1,28 +1,14 @@
-import React from 'react';
-import Typist from 'react-typist';
+import React, {useState} from 'react';
+import Typist from "react-typist";
 
 
 export default function () {
-
-
 
     return (
         <div className='app-header'>
 
             <p className='header-title'>Pratik Bassi</p>
-            <Typist>
-                <p className='subtitle'>Full Stack Developer </p>
-                <br/>
-                <a className='subtitle-link' href='https://github.com/pratikbassi'> Github </a>
-                <br/>
-                <a className='subtitle-link' href='https://www.linkedin.com/in/pratik-bassi-81a490173/'> LinkedIn </a>
-                <br/>
-                <a className='subtitle-link' href='https://resume.creddle.io/resume/h21hxcw9x6e'> Resume </a>
-                <br/>
-                <a className='subtitle-link' href='https://resume.creddle.io/resume/h21hxcw9x6e'> Projects </a>
-                <br />
-                <a className='subtitle-link' href='mailto:pratikbassi@gmail.com'> Email Me </a>
-            </Typist>
+            <p className='subtitle'>Full Stack Developer </p>
 
         </div>
     )
