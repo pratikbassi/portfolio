3713e27e611836d4a9d648b4dadbdf253551d04f
Pratik Bassi <35152777+pratikbassi@users.noreply.github.com>
2024-04-26 18:13:03 -0400

    removed old files, building new project with modern django and vue

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/email/', views.EmailCreate.as_view() ),
-    path('api/visitor/', views.VisitorListCreate.as_view()),
-
-]
---
+++ frontend/src/components/About.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+    <h2>About</h2>
+    <p>This is an about page.</p>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
