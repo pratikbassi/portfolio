befe0cbcf6915eaf09ef6a4bea2406620af22ed4
Pratik Bassi <35152777+pratikbassi@users.noreply.github.com>
2024-05-05 14:17:15 -0400

    updated components

--- client/src/components/ResumeComp.vue
+++ client/src/components/ResumeComp.vue
@@ -5,7 +5,7 @@
 <template>
   <h2>Resume</h2>
   <a href="https://www.dropbox.com/scl/fi/ow93g1wq2514jcuukahk3/Pratik-Resume-ATS.pdf?rlkey=jir52q56q1j2fb8puetnx8e75&st=w68d3vv4&dl=0">
-    This is a dropbox link to my resume. Click here to view it.
+    Dropbox link to my resume
   </a>
 </template>
 
