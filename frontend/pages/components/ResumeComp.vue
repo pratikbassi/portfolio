d5cc256556408a29b6e847fff5eb436f528a7844
Pratik Bassi <35152777+pratikbassi@users.noreply.github.com>
2025-03-07 23:44:33 -0500

    updated resume link

--- frontend/pages/components/ResumeComp.vue
+++ frontend/pages/components/ResumeComp.vue
@@ -4,7 +4,7 @@
 
 <template>
   <h2>Resume</h2>
-  <a target='_blank' href="https://www.dropbox.com/scl/fi/ow93g1wq2514jcuukahk3/Pratik-Resume-ATS.pdf?rlkey=jir52q56q1j2fb8puetnx8e75&st=w68d3vv4&dl=0">
+  <a target='_blank' href="https://www.dropbox.com/scl/fi/l3dot5zoo0pcmpvhj5ob9/Pratik-Resume-Long-ATS.pdf?rlkey=rotwykuqlp66hdf57oumiba8g&dl=0">
     Dropbox link to my resume
   </a>
 </template>
